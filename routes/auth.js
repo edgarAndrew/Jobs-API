16f8d59dd3c870c34bfba09c58202e4513154bf1
Edgar Camelo <100348698+edgarAndrew@users.noreply.github.com>
2024-11-12 21:26:06 +0530

    added /load route in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router();
 
 const {register,login} = require('../controllers/auth');
+const authMiddleware = require('../middleware/authentication');
 
 router.route('/login').post(login);
-router.route('/register').post(register)
+router.route('/register').post(register);
+router.route('/load').get(authMiddleware,async(req,res)=> res.status(StatusCodes.OK).json({"userId":req.user.userId,"username":req.user.name}));
 
 module.exports = router;
