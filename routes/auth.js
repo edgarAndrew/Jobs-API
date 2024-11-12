e62131d7745b2d2b01e8d03ff434a3b3697fe8b5
Edgar Camelo <100348698+edgarAndrew@users.noreply.github.com>
2024-11-12 21:28:09 +0530

    Update in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,6 @@ const authMiddleware = require('../middleware/authentication');
 
 router.route('/login').post(login);
 router.route('/register').post(register);
-router.route('/load').get(authMiddleware,async(req,res)=> res.status(StatusCodes.OK).json({"userId":req.user.userId,"username":req.user.name}));
+router.route('/load').get(authMiddleware,async(req,res)=> res.status(200).json({"userId":req.user.userId,"username":req.user.name}));
 
 module.exports = router;
